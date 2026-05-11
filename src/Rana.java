0287a6f69505fb57d119681e836222687ac8cddd
Mariana Correa Correa <marianawhitney456@gmail.com>
2026-05-10 18:47:11 -0600

    Adjust Rana class position and jump mechanics

--- src/Rana.java
+++ src/Rana.java
@@ -3,42 +3,95 @@ import java.awt.Graphics;
 import java.awt.Rectangle;
 
 public class Rana {
-    private int x = 100, y = 300;
-    private int velocidadY = 0;
-    private final int GRAVEDAD = 1;
-    private final int SUELO_Y = 320;
-    private boolean enSuelo = true;
 
+    private int x = 100, y = 270;
+
+    private int velocidadY = 0;
+
+    private final int GRAVEDAD = 1;
+
+    private final int SUELO_Y = 270;
+
+    // Número de saltos
+    private int saltos = 0;
+
+    // Imagen rana
+    private Image spriteRana;
+
+    public Rana() {
+
+        try {
+
+            spriteRana = new ImageIcon(
+                    getClass().getResource("/proyecto_u3/Sprites/rana.png")
+            ).getImage();
+
+        } catch (Exception e) {
+
+            System.out.println("No se pudo cargar rana.png");
+
+            e.printStackTrace();
+
+            spriteRana = null;
+        }
+    }
+
+    // Salto y doble salto
     public void saltar() {
-        if (enSuelo) {
+
+        // Máximo 2 saltos
+        if (saltos < 2) {
+
             velocidadY = -16;
-            enSuelo = false;
+
+            saltos++;
         }
     }
 
     public void actualizar() {
+
         y += velocidadY;
+
         if (y < SUELO_Y) {
+
             velocidadY += GRAVEDAD;
-            enSuelo = false;
+
         } else {
+
             y = SUELO_Y;
             velocidadY = 0;
-            enSuelo = true;
+
+            saltos = 0;
         }
     }
 
     public void dibujar(Graphics g) {
-        g.setColor(Color.GREEN);
-        g.fillRect(x, y, 30, 30);
+
+        if (spriteRana != null) {
+
+            g.drawImage(spriteRana,x,y,50,50,null);
+
+        } else {
+
+            g.fillRect(x, y, 50, 50);
+        }
     }
 
     public Rectangle getBounds() {
-        return new Rectangle(x, y, 30, 30);
+
+        return new Rectangle(x, y, 50, 50);
     }
+    public int getY() {
+
+    return y;
+}
 
     public void reset() {
+
         y = SUELO_Y;
+
         velocidadY = 0;
+
+        saltos = 0;
     }
 }
